3c0893ceb3142ecb83a5293c376198ada2a584b1
Sajzad <sajzadhasan@gmail.com>
2023-01-04 11:41:31 +0600

    wsgi updated

--- website/website/wsgi.py
+++ website/website/wsgi.py
@@ -3,6 +3,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.produc')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.production')
 
 application = get_wsgi_application()
