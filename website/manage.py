f17b3c95fbcd74b4946d32b9aa999537a44403c1
Sajzad <sajzadhasan@gmail.com>
2023-01-07 15:02:00 +0600

    updated

--- website/manage.py
+++ website/manage.py
@@ -3,8 +3,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.dev')
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.production')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
